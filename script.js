2d3eaf3db8927d4ef8ab0047fee4b45921370298
Muquit Arif <muquitarif2@gmail.com>
2022-08-19 03:44:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let coin = document.getElementById("flip-coin");
+let flip = document.getElementById("flip-again");
+let toss = Math.floor(Math.random() * 2);
+
+function flipAgain() {
+    location.reload();
+}
+
+coin.addEventListener("click", function () {
+    let result;
+
+    if (toss == 1) {
+        result = "Tails";
+        coin.src = "images/Tails.png"
+        console.log(toss);
+    } else {
+        coin.src = "images/Heads.png"
+        result = "Heads";
+    }
+
+    document.getElementById("result").innerHTML = `It's ${result}.`
+    flip.innerHTML = `Want to Flip Again?`
+    flip.addEventListener("click", flipAgain);
+    flip.style.border = "1px solid lightgrey";
+
+});
