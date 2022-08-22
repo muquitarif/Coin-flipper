23e5b95a35c6f4dd7f64d853c23fb017ee2b0c28
Muquit Arif <muquitarif2@gmail.com>
2022-08-22 19:48:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let coin = document.getElementById("flip-coin");
 let flip = document.getElementById("flip-again");
 let toss = Math.floor(Math.random() * 2);
+let tossResult = document.getElementById("result");
 
 function flipAgain() {
     location.reload();
@@ -17,7 +18,6 @@ coin.addEventListener("click", function () {
         coin.src = "images/Heads.png"
         result = "Heads";
     }
-
     tossResult.innerHTML = `It's ${result}.`
     flip.innerHTML = `<button id="flip-again-btn">Want to Flip Again?</button>`
     flip.addEventListener("click", flipAgain);
