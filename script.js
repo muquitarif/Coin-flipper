116e81d4bbc81014986975a0f8da1bf5e634526c
muhammadumar000 <FA20-BEE-150@ISBSTUDENT.COMSATS.EDU.PK>
2022-08-22 20:50:56 +0500

    coin flipping with the flip-again button.bug fixed

--- script.js
+++ script.js
@@ -4,22 +4,24 @@ let flip = document.getElementById("flip-again");
 let tossResult = document.getElementById("result");
 
 function flipAgain() {
+    coin.style.pointerEvents = 'unset';
     coin.src = "./images/Flip_coin.png"
     tossResult.innerHTML = "";
     document.getElementById('flip-again-btn').style.display = "none";
 }
 
-coin.addEventListener("click", function () {
+coin.addEventListener("click", function (event) {
     let toss = Math.floor(Math.random() * 2);
     let result;
 
     if (toss == 1) {
         result = "Tails";
-        coin.src = "images/Tails.png"
-        console.log(toss);
+        coin.src = "images/Tails.png";
+        coin.style.pointerEvents = 'none';
     } else {
         coin.src = "images/Heads.png"
         result = "Heads";
+        coin.style.pointerEvents = 'none';
     }
 
     tossResult.innerHTML = `It's ${result}.`
